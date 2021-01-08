a24fa30396a6c804edd91a6da76e59cd896f492b
Judy <jyu@lcs.on.ca>
2021-01-08 17:54:20 -0500

    add details to eyes and hands

--- Playground.playground/Pages/My name.xcplaygroundpage/Contents.swift
+++ Playground.playground/Pages/My name.xcplaygroundpage/Contents.swift
@@ -40,11 +40,23 @@ PlaygroundPage.current.liveView = canvas
 
 canvas.drawAxes(withScale: true, by: 50)
 
+canvas.drawShapesWithFill
+
+canvas.fillColor = Color.blue
+
+canvas.drawRoundedRectangle(at: Point(x: 0, y: 500), width: 300, height: 100)
+
 canvas.fillColor = Color.green
 
 canvas.drawRectangle(at: Point(x: 50, y: 75), width: 35, height: 180)
 
 canvas.drawShapesWithBorders = true
+
+canvas.fillColor = Color.black
+
+canvas.drawEllipse(at: Point(x: 80, y: 458), width: 65, height: 50)
+
+canvas.drawEllipse(at: Point(x: 218, y: 458), width: 65, height: 50)
 
 canvas.fillColor = Color.white
 
@@ -56,8 +68,17 @@ canvas.fillColor = Color.black
 
 canvas.drawEllipse(at: Point(x: 105, y: 405), width: 50, height: 40)
 
-canvas.drawCurve(from: Point(x: 138, y: 375), to: Point(x: 111, y: 352), control1: Point(x: 125, y: 364), control2: Point(x: 105, y: 353))
+canvas.drawEllipse(at: Point(x: 100, y: 325), width: 36, height: 36)
 
+canvas.drawEllipse(at: Point(x: 143, y: 375), width: 28, height: 20)
+
+canvas.drawCurve(from: Point(x: 138, y: 375), to: Point(x: 111, y: 352), control1: Point(x: 140, y: 352), control2: Point(x: 115, y: 346))
+
+canvas.drawCurve(from: Point(x: 142, y: 375), to: Point(x: 175, y: 352), control1: Point(x: 152, y: 355), control2: Point(x: 155, y: 346))
+
+canvas.fillColor = Color.white
+
+canvas.drawEllipse(at: Point (x: 111, y: 405), width: 13, height: 8)
 
 /*:
  ## Show the Live View
