071bd40e3100134fa37e67c192573c874649e459
Judy <jyu@lcs.on.ca>
2021-01-21 14:34:40 -0500

    start Pixies Poster

--- Playground.playground/Pages/Pixies Poster.xcplaygroundpage/Contents.swift
+++ Playground.playground/Pages/Pixies Poster.xcplaygroundpage/Contents.swift
@@ -4,7 +4,7 @@
  
  Set the size of your desired canvas by adjusting the constants on lines 19 and 20.
  */
-let preferredWidth = 600
+let preferredWidth = 400
 let preferredHeight = 600
 /*:
  ## Required code
@@ -37,7 +37,34 @@ PlaygroundPage.current.liveView = canvas
  */
 
 // Replace this comment with your first comment – what is the goal of the code you're about to write?
-canvas.drawRectangle(at: Point(x: 50, y: 75), width: 100, height: 200)
+canvas.drawAxes(withScale: true,
+                by: 50)
+
+canvas.drawShapesWithFill = true
+canvas.drawShapesWithBorders = false
+    
+let offWhite = Color(hue: 81,
+                     saturation: 5,
+                     brightness: 88,
+                     alpha: 100)
+
+let limeGreen = Color(hue: 106,
+                      saturation: 64,
+                      brightness: 73,
+                      alpha: 100)
+
+//background
+//set color
+canvas.fillColor = Color.init(hue: 106, saturation: 64, brightness: 73, alpha: 100)
+
+canvas.drawRectangle(at: Point(x: 0, y: 0),
+                     width: preferredWidth,
+                     height: preferredHeight)
+
+
+
+
+
 
 /*:
  ## Show the Live View
