cb0d4a7b8bb673e8f0a592a69038c890339174a8
Judy <jyu@lcs.on.ca>
2021-01-26 19:48:10 -0500

    create repeating dragon pattern

--- Playground.playground/Pages/Poster design.xcplaygroundpage/Contents.swift
+++ Playground.playground/Pages/Poster design.xcplaygroundpage/Contents.swift
@@ -39,22 +39,67 @@ PlaygroundPage.current.liveView = canvas
 // Replace this comment with your first comment – what is the goal of the code you're about to write?
 
 //set colors
-let darkorange = Color.init(hue: 140, saturation: 60, brightness: 70, alpha: 100)
-//let greenblue =
+let lightorange = Color.init(hue: 38, saturation: 100, brightness: 85, alpha: 100)
+let greenblue = Color.init(hue: 150, saturation: 60, brightness: 70, alpha: 100)
+let green = Color.init(hue: 110, saturation: 60, brightness: 70, alpha: 100)
+let darkorange = Color.init(hue: 25, saturation: 80, brightness: 80, alpha: 100)
 
-canvas.drawRectangle(at: Point(x: 0, y: 0), width: preferredWidth, height: preferredHeight)
-//first
-canvas.fillColor = Color.init(hue: 150, saturation: 60, brightness: 70, alpha: 100)
-canvas.drawRectangle(at: Point(x: 100, y: 100), width: 50, height: 50)
-//second
-canvas.fillColor = Color.init(hue: 110, saturation: 60, brightness: 70, alpha: 100)
-canvas.drawRectangle(at: Point(x: 150, y: 150), width: 50, height: 50)
-//third
-canvas.fillColor = Color.init(hue: 25, saturation: 80, brightness: 80, alpha: 100)
-canvas.drawRectangle(at: Point(x: 200, y: 200), width: 50, height: 50)
-//fourth
-canvas.fillColor = Color.init(hue: 35, saturation: 80, brightness: 85, alpha: 100)
-canvas.drawRectangle(at: Point(x: 250, y: 250), width: 50, height: 50)
+//repeat dragon pattern
+for z in stride(from: 0, through: 300, by: 100){
+    for y in stride(from: 0, through: 300, by: 100){
+        //if
+        //else
+        canvas.fillColor = Color.init(hue: 25, saturation: 80, brightness: 80, alpha: 100)
+        
+        var dragonVertices: [Point] = []
+        dragonVertices.append(Point(x: z + 16, y: y + 53))
+        dragonVertices.append(Point(x: z + 22, y: y + 62))
+        dragonVertices.append(Point(x: z + 30, y: y + 68))
+        dragonVertices.append(Point(x: z + 31, y: y + 76))
+        dragonVertices.append(Point(x: z + 38, y: y + 74))
+        dragonVertices.append(Point(x: z + 60, y: y + 92))
+        dragonVertices.append(Point(x: z + 46, y: y + 72))
+        dragonVertices.append(Point(x: z + 57, y: y + 80))
+        dragonVertices.append(Point(x: z + 49, y: y + 70))
+        dragonVertices.append(Point(x: z + 55, y: y + 72))
+        dragonVertices.append(Point(x: z + 50, y: y + 66))
+        dragonVertices.append(Point(x: z + 49, y: y + 61))
+        dragonVertices.append(Point(x: z + 47, y: y + 56))
+        dragonVertices.append(Point(x: z + 69, y: y + 71))
+        dragonVertices.append(Point(x: z + 70, y: y + 84))
+        dragonVertices.append(Point(x: z + 67, y: y + 97))
+        dragonVertices.append(Point(x: z + 75, y: y + 90))
+        dragonVertices.append(Point(x: z + 80, y: y + 69))
+        dragonVertices.append(Point(x: z + 83, y: y + 26))
+        dragonVertices.append(Point(x: z + 72, y: y + 41))
+        dragonVertices.append(Point(x: z + 65, y: y + 43))
+        dragonVertices.append(Point(x: z + 59, y: y + 40))
+        dragonVertices.append(Point(x: z + 51, y: y + 30))
+        dragonVertices.append(Point(x: z + 54, y: y + 25))
+        dragonVertices.append(Point(x: z + 69, y: y + 29))
+        dragonVertices.append(Point(x: z + 67, y: y + 15))
+        dragonVertices.append(Point(x: z + 70, y: y + 10))
+        dragonVertices.append(Point(x: z + 18, y: y + 2))
+        dragonVertices.append(Point(x: z + 68, y: y + 8))
+        dragonVertices.append(Point(x: z + 65, y: y + 12))
+        dragonVertices.append(Point(x: z + 64, y: y + 22))
+        dragonVertices.append(Point(x: z + 39, y: y + 14))
+        dragonVertices.append(Point(x: z + 31, y: y + 23))
+        dragonVertices.append(Point(x: z + 40, y: y + 52))
+        dragonVertices.append(Point(x: z + 39, y: y + 58))
+        dragonVertices.append(Point(x: z + 35, y: y + 60))
+        dragonVertices.append(Point(x: z + 21, y: y + 55))
+        dragonVertices.append(Point(x: z + 19, y: y + 50))
+        dragonVertices.append(Point(x: z + 16, y: y + 53))
+        
+        canvas.drawCustomShape(with: dragonVertices)
+        
+    }
+}
+
+
+
+//text
 
 
 canvas.drawAxes(withScale: true, by: 50, color: .white)
--- Playground.playground/Pages/The Velvet Underground Poster.xcplaygroundpage/Contents.swift
+++ Playground.playground/Pages/The Velvet Underground Poster.xcplaygroundpage/Contents.swift
@@ -37,25 +37,85 @@ PlaygroundPage.current.liveView = canvas
  */
 
 // Replace this comment with your first comment – what is the goal of the code you're about to write?
-//canvas.drawRectangle(at: Point(x: 0, y: 0), width: preferredWidth, height: preferredHeight)
+canvas.drawRectangle(at: Point(x: 0, y: 0), width: preferredWidth, height: preferredHeight)
 
-canvas.drawAxes(withScale: true, by: 50)
+//set color
+let purple = Color.init(hue: 309, saturation: 78, brightness: 64, alpha: 100)
+let offwhite = Color.init(hue: 83, saturation: 4, brightness: 89, alpha: 100)
 
-for s in stride(from: 0, through: 400, by: 80){
-    for y in stride(from: 0, through: 400, by: 80){
-        //if
-        let purple = Color
-        //else
+// set fill
+canvas.drawShapesWithFill = true
+
+//set border
+canvas.drawShapesWithBorders = false
+
+for s in stride(from: 0, to: 400, by: 80){
+    for y in stride(from: 0, to: 400, by: 80){
+        if s == y {
+            canvas.fillColor = Color.init(hue: 83, saturation: 4, brightness: 89, alpha: 100)
+        }
+        else {
+            canvas.fillColor = Color.init(hue: 309, saturation: 78, brightness: 64, alpha: 100)
+        }
         var arrowVertices: [Point] = []
         arrowVertices.append(Point(x: s, y: y))
-        arrowVertices.append(Point(x: s, y: y))
-        arrowVertices.append(Point(x: s, y: y))
-        arrowVertices.append(Point(x: s, y: y))
+        arrowVertices.append(Point(x: s, y: y + 65))
+        arrowVertices.append(Point(x: s + 16, y: y + 65))
+        arrowVertices.append(Point(x: s + 16, y: y + 27))
+        arrowVertices.append(Point(x: s + 69, y: y + 80))
+        arrowVertices.append(Point(x: s + 80, y: y + 69))
+        arrowVertices.append(Point(x: s + 28, y: y + 16))
+        arrowVertices.append(Point(x: s + 65, y: y + 16))
+        arrowVertices.append(Point(x: s + 65, y: y))
         arrowVertices.append(Point(x: s, y: y))
         canvas.drawCustomShape(with: arrowVertices)
     }
 }
+//lines
+canvas.defaultLineWidth = 1
+canvas.lineColor = Color.init(hue: 83, saturation: 4, brightness: 89, alpha: 100)
 
+canvas.drawLine(from: Point(x: 0, y: 465), to: Point(x: 400, y: 465))
+canvas.drawLine(from: Point(x: 0, y: 570), to: Point(x: 400, y: 570))
+
+//text
+canvas.textColor = Color.init(hue: 83, saturation: 4, brightness: 89, alpha: 100)
+canvas.drawText(message: "the velvet underground",
+                at: Point(x: 12, y: 410),
+                size: 32,
+                kerning: -1)
+
+canvas.drawText(message: "first appearance in london",
+                at: Point(x: 11, y: 550),
+                size: 9,
+                kerning: -0.5)
+
+canvas.drawText(message: "the london college of printing",
+                at: Point(x: 11, y: 540),
+                size: 9,
+                kerning: -0.5)
+
+canvas.drawText(message: "with spring and pollyfloskin",
+                at: Point(x: 150, y: 550),
+                size: 9,
+                kerning: -0.5)
+
+canvas.drawText(message: "plus the great western light show",
+                at: Point(x: 150, y: 540),
+                size: 9,
+                kerning: -0.5)
+
+canvas.drawText(message: "thursday",
+                at: Point(x: 300, y: 550),
+                size: 9,
+                kerning: -0.5)
+
+canvas.drawText(message: "october 14 1971 / 8pm",
+                at: Point(x: 300, y: 540),
+                size: 9,
+                kerning: -0.5)
+
+//canvas.drawAxes(withScale: true, by: 50, color: .white)
 
 
 
